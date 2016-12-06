a99cdf6bb68b742352d4a4c58710dbe802189b6a
ShineunYoon <syoon19@hawk.iit.edu>
2016-12-06 05:22:50 -0600

    adjusted indent

--- js/site.js
+++ js/site.js
@@ -1,30 +1,32 @@
+
 var tag = document.createElement("script");
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
-function onYouTubeIframeAPIReady(){
-	player = new YT.player("player",{
-	height: '360',
-	width: '640',
-	playerVars: {
-		"autoplay":0,
-		"controls":1
-	},
-	events: {
-		"onReady": onPlayerReady
-	}
+
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player("player", {
+	  height: '360',
+	  width: '640',
+	  playerVars: { 
+	  	"autoplay": 0, 
+	  	"controls": 1
+	  },
+	  events: {
+	    "onReady": onPlayerReady
+	  }
 	});
 }
-
-function onPlayerReady(event){
-	player.cuePlaylist({
-  listType:"playlist",
-  list:"PL8zglt-LDl-iwBHEl3Pw1IhWGp9cfgMrc",
-  index:"0"
+      
+function onPlayerReady(event) {
+  player.cuePlaylist({
+	  listType:"playlist",
+	  list:"PL8zglt-LDl-iwBHEl3Pw1IhWGp9cfgMrc",
+	  index:"0"
   });
 }
-
+      
 function nextVideo(){
-	player.nextVideo();
-}
+  player.nextVideo();
+}            
